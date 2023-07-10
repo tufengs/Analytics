83da58596db09867e35c5af9c0d994b9a4fb5a0b
Maxime MARCHAND <mmarchand6@myges.fr>
2023-07-10 23:44:41 +0200

    Improve SDK

---
+++ sdk/src/enums/EventEnum.ts
@@ -0,0 +1,9 @@
+export enum EventEnum {
+    click = 'click',
+    mouse = 'mouse',
+    navigation = 'navigation',
+    drag = 'drag',
+    keyboard = 'keyboard',
+    idle = 'idle',
+    hover = 'hover',
+}
