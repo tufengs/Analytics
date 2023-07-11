5c5504e8f137e78ecb3415612b8743792aedcbbc
Antoine lin <lin.antoine.pro@gmail.com>
2023-07-11 08:49:11 +0200

    add page, and path

---
+++ backoffice/src/components/structure/Sidebar.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        permanent
+        theme="dark"
+      >
+        <v-list nav>
+          <v-list-item
+            v-for="path in admin_path"
+            :prepend-icon="path.icon"
+            :title="path.title"
+            :value="path.value"
+            :to="path.to"
+          />
+          <v-list-item
+            v-for="path in webmaster_path"
+            :prepend-icon="path.icon"
+            :title="path.title"
+            :value="path.value"
+            :to="path.to"
+          />
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script setup lang='ts'>
+
+import {ref} from "vue";
+
+const admin_path = ref([
+  {
+    icon: '',
+    title: 'Webmasters',
+    value: 'webmasters',
+    to: 'admin-webmasters',
+  },
+  {
+    icon: '',
+    title: 'Webmasters Request',
+    value: 'webmasters-request',
+    to: 'admin-webmasters-request',
+  }
+])
+
+const webmaster_path = ref([
+  {
+    icon: '',
+    title: 'Dashboard',
+    value: 'dashboard',
+    to: 'webmaster-dashboard',
+  },
+  {
+    icon: '',
+    title: 'Funnels',
+    value: 'funnels',
+    to: 'webmaster-funnels',
+  },
+  {
+    icon: '',
+    title: 'Tags',
+    value: 'tags',
+    to: 'webmaster-tags',
+  },
+  {
+    icon: '',
+    title: 'Profile',
+    value: 'profile',
+    to: 'webmaster-profile',
+  }
+])
+</script>
+
+<style scoped>
+
+</style>
