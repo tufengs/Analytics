97cb3a73a063d2b140ffb5fa9e518c437db66a70
Antoine Pollet <antoine.pollet@idcapture.fr>
2023-07-15 18:25:03 +0200

    Forgot JwtService

--- api/src/tag/tag.module.ts
+++ api/src/tag/tag.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
 import { PrismaService } from 'src/prisma.service';
-
+import { JwtService } from '@nestjs/jwt';
 @Module({
   controllers: [TagController],
-  providers: [TagService, PrismaService]
+  providers: [TagService, JwtService, PrismaService]
 })
 export class TagModule {}
