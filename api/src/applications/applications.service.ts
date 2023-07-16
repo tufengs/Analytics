696ae9c9d9fe871acd020215df9d6ef1a60d7594
Maxime MARCHAND <mmarchand6@myges.fr>
2023-07-16 15:20:17 +0200

    SecretGuards and Prisma Schema

--- api/src/applications/applications.service.ts
+++ api/src/applications/applications.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaService} from "../prisma.service";
+import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class ApplicationsService {
-    constructor(private prismaService: PrismaService) {}
+    constructor(private prismaService: PrismaService) { }
 
-    async findAll(){
+    async findAll() {
         return this.prismaService.application.findMany()
     }
+
+    // async findOneWithSecret(id: string, secret_id: string) {
+    //     return this.prismaService.application.findFirst({
+    //         where: {
+    //             id: id,
+    //             credentials:
+    //             {
+    //                 id: secret_id
+    //             }
+    //         }
+    //     })
+    // }
 }
